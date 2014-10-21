fea52adf5cff0efc1b504441c9eb807850fee867
Aleksandr Karasev <xsandr.k@gmail.com>
2014-10-21 10:47:59 +0400

    initial

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='Xlsxcursor',
+    description="Xlsxcursor for xlsxwriter.",
+    version='0.1',
+    packages=['xlsxcursor'],
+    install_requires=[
+        'xlsxwriter',
+    ],
+)
