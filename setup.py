8452fd19a6968d2e5a105acd8e700b1b7f57689a
Aleksandr Karasev <xsandr.k@gmail.com>
2014-10-21 13:28:12 +0400

    new version 0.1.4

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from setuptools import setup
 
 
-VERSION = "0.1.3"
+VERSION = "0.1.4"
 
 setup(
     name='Xlsxcursor',
     description="Xlsxcursor for xlsxwriter.",
     version=VERSION,
+    url='https://github.com/KokocGroup/xslxcursor',
+    download_url='https://github.com/KokocGroup/xslxcursor/tarball/v{}'.format(VERSION),
     packages=['xlsxcursor'],
     install_requires=[
         'xlsxwriter',
